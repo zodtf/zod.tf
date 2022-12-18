05ff3acf2a1eb8b24714c5bfa00b49c4984c4859
jason <jason.p.mcelhenney@gmail.com>
2022-12-18 06:10:38 -0600

    add "sendToLive.sh" for NGiNX Deploy
    little script I use for my sites which is useful here.

---
+++ sendToLive.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo 'Updating /var/www/ with new data...';
+
+cp -R ./$1 /var/www/;
+cd /var/www/$1;
+chmod 777 fixPerms.sh;
+sudo ./fixPerms.sh;
+sudo systemctl restart nginx;
+echo 'Restarted NGINX -- showing status.';
+sudo systemctl status nginx;
